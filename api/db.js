f21af0a1ccefb6d2c5f6119918fed17d405df7b4
abhishek10ghosh <abhishek10ghosh@gmail.com>
2024-07-05 23:27:41 +0530

    added login and register functionalities

--- api/db.js
+++ api/db.js
@@ -1,8 +1,15 @@
-import mysql from "mysql";
+import mysql from "mysql2";
 
 export const db = mysql.createConnection({
     host:"localhost",
     user:"root",
     password:"123456",
     database:"blog"
-})
+});
+
+db.connect((err) => {
+    if (err) return console.error(err.message);
+  
+    console.log('Connected to the MySQL server.');
+  });
+
